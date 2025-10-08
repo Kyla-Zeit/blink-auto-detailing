4ab59d9577cbabe408e8480e5a50dbf308c0fb87
Kyla <112965597+Kyla-Zeit@users.noreply.github.com>
2025-10-08 03:53:36 +0000

    R

--- components/header.tsx
+++ components/header.tsx
@@ -33,7 +33,7 @@ export function Header() {
                 src={blinkLogo}
                 alt="Blink Auto Detailing"
                 priority
-                className="max-h-[calc(var(--nav-h)-50px)] w-auto object-contain"
+                className="max-h-[calc(var(--nav-h)-2px)] w-auto object-contain"
               />
             </Link>
 
